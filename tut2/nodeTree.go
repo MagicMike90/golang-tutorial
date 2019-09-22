868f163b231308a4a97ff94d71a07367f7845103
Michael Luo <michael.luo0801@gmail.com>
2019-09-22 12:30:17 +1000

    update

--- tut2/nodeTree.go
+++ tut2/nodeTree.go
@@ -4,14 +4,6 @@ import (
 	"fmt"
 )
 
-func functionOne(x int) {
-	fmt.Println(x)
-}
-
 func main() {
-	varOne := 1
-	varTwo := 2
 	fmt.Println("Hello there!")
-	functionOne(varOne)
-	functionOne(varTwo)
 }
