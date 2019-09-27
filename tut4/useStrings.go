e4d49f192df4ae52273bbeb7387335929403f469
Michael Luo <michael.luo0801@gmail.com>
2019-09-27 23:48:35 +1000

    use build in string function

---
+++ tut4/useStrings.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+	"unicode"
+)
+
+// You should not overuse this feature because you might end up having difficult to read code!
+var f = fmt.Printf
+
+func main() {
+	upper := s.ToUpper("Hello there!")
+	f("To Upper: %s\n", upper)
+	f("To Lower: %s\n", s.ToLower("Hello THERE"))
+
+	f("%s\n", s.Title("tHis wiLL be A title!"))
+
+	// strings.EqualFold() function allows you to determine whether two strings are the same in spite of their differences in their letters.
+	f("EqualFold: %v\n", s.EqualFold("Mihalis", "MIHAlis"))
+	f("EqualFold: %v\n", s.EqualFold("Mihalis", "MIHAli"))
+
+	// Returns true when the first parameter string begins with the second parameter string and false otherwise
+	f("Prefix: %v\n", s.HasPrefix("Mihalis", "Mi"))
+	f("Prefix: %v\n", s.HasPrefix("Mihalis", "mi"))
+	f("Suffix: %v\n", s.HasSuffix("Mihalis", "is"))
+	f("Suffix: %v\n", s.HasSuffix("Mihalis", "IS"))
+
+	f("Index: %v\n", s.Index("Mihalis", "ha"))
+	f("Index: %v\n", s.Index("Mihalis", "Ha"))
+	// counts the number of non-overlapping times the second parameter appears in the string that is given as the first parameter
+	f("Count: %v\n", s.Count("Mihalis", "i"))
+	f("Count: %v\n", s.Count("Mihalis", "I"))
+	f("Repeat: %s\n", s.Repeat("ab", 5))
+
+	f("TrimSpace: %s\n", s.TrimSpace(" \tThis is a line. \n"))
+	f("TrimLeft: %s", s.TrimLeft(" \tThis is a\t line. \n", "\n\t "))
+	f("TrimRight: %s\n", s.TrimRight(" \tThis is a\t line. \n", "\n\t "))
+
+	f("Compare: %v\n", s.Compare("Mihalis", "MIHALIS"))
+	f("Compare: %v\n", s.Compare("Mihalis", "Mihalis"))
+	f("Compare: %v\n", s.Compare("MIHALIS", "MIHalis"))
+
+	//    splits the string parameter using white space characters as separators
+	f("Fields: %v\n", s.Fields("This is a string!"))
+	f("Fields: %v\n", s.Fields("Thisis\na\tstring!"))
+
+	f("%s\n", s.Split("abcd efg", ""))
+	// The first one is the string that you want to process. The second parameter contains the string that, if found, will be replaced by the third parameter
+	f("%s\n", s.Replace("abcd efg", "", "_", -1))
+	f("%s\n", s.Replace("abcd efg", "", "_", 4))
+	f("%s\n", s.Replace("abcd efg", "", "_", 2))
+
+	lines := []string{"Line 1", "Line 2", "Line 3"}
+	f("Join: %s\n", s.Join(lines, "+++"))
+
+	// splits its first parameter string into substrings based on the separator string that is given as the second parameter to the function
+	f("SplitAfter: %s\n", s.SplitAfter("123++432++", "++"))
+
+	trimFunction := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+	f("TrimFunc: %s\n", s.TrimFunc("123 abc ABC \t .", trimFunction))
+}
