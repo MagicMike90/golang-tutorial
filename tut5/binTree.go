405eebd03c02cb548a9378419d6381b3af492842
Michael Luo <michael.luo0801@gmail.com>
2019-09-28 10:00:58 +1000

    add binary tree

---
+++ tut5/binTree.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Tree struct {
+	Left  *Tree
+	Value int
+	Right *Tree
+}
+
+func traverse(t *Tree) {
+	if t == nil {
+		return
+	}
+	traverse(t.Left)
+	fmt.Print(t.Value, " ")
+	traverse(t.Right)
+}
+func insert(t *Tree, v int) *Tree {
+	// If it is indeed an empty tree, then the new node will be the root of the tree and it will be created
+	if t == nil {
+		return &Tree{nil, v, nil}
+	}
+
+	// determines whether the value you are trying to insert already exists in the binary tree or not
+	if v == t.Value {
+		return t
+	}
+
+	if v < t.Value {
+		t.Left = insert(t.Left, v)
+		return t
+	}
+	t.Right = insert(t.Right, v)
+	return t
+}
+
+func create(n int) *Tree {
+	var t *Tree
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < 2*n; i++ {
+		temp := rand.Intn(n * 2)
+		t = insert(t, temp)
+	}
+	return t
+}
+func main() {
+	tree := create(10)
+	fmt.Println("The value of the root of the tree is", tree.Value)
+	traverse(tree)
+	fmt.Println()
+	tree = insert(tree, -10)
+	tree = insert(tree, -2)
+	traverse(tree)
+	fmt.Println()
+	fmt.Println("The value of the root of the tree is", tree.Value)
+}
