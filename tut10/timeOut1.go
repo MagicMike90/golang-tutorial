842a58519d893008733ae78b5df7c916a2f99093
Michael Luo <michael.luo0801@gmail.com>
2019-11-12 23:20:11 +1100

    add goroutine

---
+++ tut10/timeOut1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+
+	// nonymous function that is executed as a goroutine will take about 3 seconds
+	go func() {
+		time.Sleep(time.Second * 3)
+		c1 <- "c1 OK"
+	}()
+
+	// wait for the chosen time
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(time.Second * 1):
+		fmt.Println("timeout c1")
+	}
+
+	c2 := make(chan string)
+	go func() {
+		time.Sleep(3 * time.Second)
+		c2 <- "c2 OK"
+	}()
+
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(4 * time.Second):
+		fmt.Println("timeout c2")
+	}
+}
